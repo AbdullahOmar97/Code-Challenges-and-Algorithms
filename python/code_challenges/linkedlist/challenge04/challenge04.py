ed8adcecb7d75a24cdf0184da1b557d24cfd7819
AbdullahOmar97 <AbdullahOmar@outlook.com>
2024-06-27 11:04:34 +0000

    add docstring

--- python/code_challenges/linkedlist/challenge04/challenge04.py
+++ python/code_challenges/linkedlist/challenge04/challenge04.py
@@ -1,11 +1,28 @@
-# challenge04.py
-
 class ListNode:
+    """
+    Definition for a singly linked list node.
+    """
     def __init__(self, val=0, next=None):
+        """
+        Initialize a ListNode with a value and optional next node.
+
+        Args:
+            val (int): Value to be stored in the node. Defaults to 0.
+            next (ListNode, optional): Reference to the next ListNode. Defaults to None.
+        """
         self.val = val
         self.next = next
 
 def reverseLinkedList(head: ListNode) -> ListNode:
+    """
+    Reverse a singly linked list in-place.
+
+    Args:
+        head (ListNode): Head of the linked list to be reversed.
+
+    Returns:
+        ListNode: Head of the reversed linked list.
+    """
     prev = None
     current = head
     
@@ -18,6 +35,15 @@ def reverseLinkedList(head: ListNode) -> ListNode:
     return prev  # prev will be the new head
 
 def createLinkedList(arr):
+    """
+    Create a singly linked list from a list of values.
+
+    Args:
+        arr (list): List of values to be stored in the linked list nodes.
+
+    Returns:
+        ListNode: Head of the created linked list.
+    """
     if not arr:
         return None
     head = ListNode(arr[0])
@@ -28,6 +54,15 @@ def createLinkedList(arr):
     return head
 
 def printLinkedList(head):
+    """
+    Print values of a singly linked list.
+
+    Args:
+        head (ListNode): Head of the linked list to be printed.
+
+    Returns:
+        list: List of values in the linked list.
+    """
     arr = []
     while head:
         arr.append(head.val)
